a87b7d7e3d8d64a3f49a6c483dbe36ac3e46f60f
Subhitsha123 <74136920+Subhitsha123@users.noreply.github.com>
2022-06-15 12:32:47 +0800

    Add files via upload

---
+++ vegies.py
@@ -0,0 +1,31 @@
+def find_vegies():
+    
+    print("Vegetable shopping made easier!")
+
+    vegie = input('Enter the vegetable you want to buy:')
+
+    if vegie == "carrot":
+        print("It is long and orange in color")
+
+    if vegie == "tomato":
+        print("It is round and red in color") 
+
+    if vegie == "beans":
+        print("It is thin, long and green in color")
+
+    if vegie == "cabbage":
+        print("It is round,leafy and green in color")
+
+    if vegie == "onion":
+        print("It is round and white,yellow or red with easily peelable skin") 
+
+    if vegie == "broccoli":
+        print("It is green and looks like it is filled with tiny granules")
+
+    if vegie == "potato":
+        print("It is yellow, size of your fist and has small depressions called eyes")
+
+    if vegie == "peas":
+        print("It is green, resembles beans but smaller")                            
+
+find_vegies()   
